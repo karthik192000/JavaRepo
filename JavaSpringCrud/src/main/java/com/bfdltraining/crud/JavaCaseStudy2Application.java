4cf973869bf7dd134607d0dea14bae3a497343c4
Karthik Bankapur <bankapurkarthik@gmail.com>
2022-02-03 21:26:02 +0530

    First Commit

---
+++ JavaSpringCrud/src/main/java/com/bfdltraining/crud/JavaCaseStudy2Application.java
@@ -0,0 +1,13 @@
+package com.bfdltraining.crud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaCaseStudy2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JavaCaseStudy2Application.class, args);
+	}
+
+}
---
+++ JavaSpringCrud/src/test/java/com/bfdltraining/crud/JavaCaseStudy2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bfdltraining.crud;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavaCaseStudy2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
