4850db4cd48b1d258d7cb8214b3781f549e71e33
Karthik Bankapur <bankapurkarthik@gmail.com>
2022-02-03 21:25:31 +0530

    Delete JavaSpringCrud directory

--- JavaSpringCrud/src/main/java/com/bfdltraining/crud/repos/ProductRepository.java
+++
@@ -1,9 +0,0 @@
-package com.bfdltraining.crud.repos;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.bfdltraining.crud.Entities.Products;
-
-public interface ProductRepository extends JpaRepository<Products, Integer> {
-
-}
